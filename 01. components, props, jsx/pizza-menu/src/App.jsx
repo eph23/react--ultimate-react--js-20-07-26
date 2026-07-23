975b22c8712f4dcc0c1d33607605d67695254ed0
Eph23 <ephraims.818@gmail.com>
2026-07-23 19:15:26 +0600

    REACT--ULTIMATE-COURSE - Components, Props, JSX - 03. JavaScript Logic in Components

--- 01. components, props, jsx/pizza-menu/src/App.jsx
+++ 01. components, props, jsx/pizza-menu/src/App.jsx
@@ -22,6 +22,11 @@ function Menu() {
 }
 
 function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+
     return (
         <footer>{new Date().toLocaleTimeString()} We are currently open</footer>
     );
