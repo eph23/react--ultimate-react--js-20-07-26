f1c0c813891c67fb0bd25330f5027a0084c0d4e8
Eph23 <ephraims.818@gmail.com>
2026-07-25 02:26:42 +0600

    REACT--ULTIMATE-COURSE - Components, Props, JSX - 08. Conditional Rendering With Ternaries

--- 01. components, props, jsx/pizza-menu/src/App.jsx
+++ 01. components, props, jsx/pizza-menu/src/App.jsx
@@ -22,20 +22,22 @@ function Pizza(props) {
 }
 
 function Menu() {
-    const pizzas = pizzaData;
-    // const pizzas = [];
+    // const pizzas = pizzaData;
+    const pizzas = [];
     const numPizzas = pizzas.length;
 
     return (
         <main className="menu">
             <h2>Our Menu</h2>
 
-            {numPizzas > 0 && (
+            {numPizzas > 0 ? (
                 <ul className="pizzas">
                     {pizzaData.map((pizza) => (
                         <Pizza pizzaObj={pizza} key={pizza.name} />
                     ))}
                 </ul>
+            ) : (
+                <p>We are still working on our menu. Please come back later!</p>
             )}
         </main>
     );
@@ -52,7 +54,7 @@ function Footer() {
             {isOpen && (
                 <div className="order">
                     <p>
-                        We are open until {closeHour}:00. COme visit us or order
+                        We are open until {closeHour}:00. Come visit us or order
                         online
                     </p>
                     <div className="btn">Order</div>
