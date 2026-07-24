9b89c7996631de33231f99bf70ee07ee7134443c
Eph23 <ephraims.818@gmail.com>
2026-07-25 02:21:48 +0600

    REACT--ULTIMATE-COURSE - Components, Props, JSX - 07. Conditional Rendering With && Operator

--- 01. components, props, jsx/pizza-menu/src/App.jsx
+++ 01. components, props, jsx/pizza-menu/src/App.jsx
@@ -1,4 +1,4 @@
-import pizaaData from "./data.js";
+import pizzaData from "./data.js";
 
 function Header() {
     return (
@@ -22,27 +22,42 @@ function Pizza(props) {
 }
 
 function Menu() {
+    const pizzas = pizzaData;
+    // const pizzas = [];
+    const numPizzas = pizzas.length;
+
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            <ul className="pizzas">
-                {pizaaData.map((pizza) => (
-                    <Pizza pizzaObj={pizza} key={pizza.name} />
-                ))}
-            </ul>
+
+            {numPizzas > 0 && (
+                <ul className="pizzas">
+                    {pizzaData.map((pizza) => (
+                        <Pizza pizzaObj={pizza} key={pizza.name} />
+                    ))}
+                </ul>
+            )}
         </main>
     );
 }
 
 function Footer() {
     const hour = new Date().getHours();
-    const openHour = 12;
+    const openHour = 0;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
 
     return (
         <footer className="footer">
-            {new Date().toLocaleTimeString()} We are currently open
+            {isOpen && (
+                <div className="order">
+                    <p>
+                        We are open until {closeHour}:00. COme visit us or order
+                        online
+                    </p>
+                    <div className="btn">Order</div>
+                </div>
+            )}
         </footer>
     );
 }
