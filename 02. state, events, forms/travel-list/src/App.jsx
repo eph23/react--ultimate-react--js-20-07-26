5165600b2469fe3b54c6b9dae0b11ca32fdc02ac
Eph23 <ephraims.818@gmail.com>
2026-07-31 03:22:37 +0600

    REACT--ULTIMATE-COURSE - State, Events, Forms - Travel List - 01. Building the Layout

--- 02. state, events, forms/travel-list/src/App.jsx
+++ 02. state, events, forms/travel-list/src/App.jsx
@@ -1,7 +1,45 @@
-
-function App() {
-
-  return <h1>Hello</h1>
+function Logo() {
+    return <h1>🌴Far Away🧳</h1>;
 }
 
-export default App
+function Form() {
+    return (
+        <div className="add-form">
+            <h3>What do you need for your trip?</h3>
+            <form>
+                <input type="text" />
+                <select>
+                    <option>1</option>
+                </select>
+                <button>Add Item</button>
+            </form>
+        </div>
+    );
+}
+
+function PackingList() {
+    return <div className="list"></div>;
+}
+
+function State() {
+    return (
+        <footer className="stats">
+            <em>
+                You have X items on your list, and you already packed X (X%)
+            </em>
+        </footer>
+    );
+}
+
+function App() {
+    return (
+        <div className="app">
+            <Logo />
+            <Form />
+            <PackingList />
+            <State />
+        </div>
+    );
+}
+
+export default App;
