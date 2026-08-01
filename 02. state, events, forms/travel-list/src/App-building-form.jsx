e5141b2113f609cb0a603d8af95d9f02b43c5965
Eph23 <ephraims.818@gmail.com>
2026-08-01 17:25:24 +0600

    REACT--ULTIMATE-COURSE - State, Events, Forms - Travel List - 04. Controlled Elements

--- 02. state, events, forms/travel-list/src/App-building-form.jsx
+++ 02. state, events, forms/travel-list/src/App-building-form.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const initialItems = [
     { id: 1, description: "Passports", quantity: 2, packed: false },
     { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -11,40 +9,16 @@ function Logo() {
 }
 
 function Form() {
-    const [description, setDescription] = useState("");
-    const [quantity, setQuantity] = useState(1);
-
     function handleSubmit(event) {
         event.preventDefault();
-
-        if (!description) return;
-
-        const newItem = {
-            description,
-            quantity,
-            packed: false,
-            id: Date.now(),
-        };
-
-        console.log(newItem);
-
-        setDescription("");
-        setQuantity(1);
-    }
-
-    function handleChange(event) {
-        setDescription(event.target.value);
-    }
-    function handleSelect(event) {
-        console.log(event.target.value);
-        setQuantity(Number(event.target.value));
+        console.log(event)
     }
 
     return (
         <div className="add-form" onSubmit={handleSubmit}>
             <form className="add-form">
                 <h3>What do you need for your trip?</h3>
-                <select value={quantity} onChange={handleSelect}>
+                <select>
                     {Array.from({ length: 20 }, (_, index) => index + 1).map(
                         (num) => (
                             <option value={num} key={num}>
@@ -53,12 +27,7 @@ function Form() {
                         ),
                     )}
                 </select>
-                <input
-                    type="text"
-                    placeholder="Item..."
-                    value={description}
-                    onChange={handleChange}
-                />
+                <input type="text" placeholder="Item..." />
                 <button>Add Item</button>
             </form>
         </div>
