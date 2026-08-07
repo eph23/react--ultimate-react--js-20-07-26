15805be455a66ee14cd7bec979ee9220305fb235
Eph23 <ephraims.818@gmail.com>
2026-08-08 03:32:41 +0600

    REACT--ULTIMATE-COURSE - TIR-State Management - 09. More Reusability With Children Prop

--- 03. TIR-state management/steps-component/src/App-05-children-prop.jsx
+++ 03. TIR-state management/steps-component/src/App-05-children-prop.jsx
@@ -6,15 +6,6 @@ const messages = [
     "Invest your new income 🤑",
 ];
 
-function StepMessage({ step, children }) {
-    return (
-        <div className="message">
-            <h3>Step {step}</h3>
-            {children}
-        </div>
-    );
-}
-
 function Button({ textColor, backgroundColor, onClick, children }) {
     return (
         <button
@@ -61,9 +52,9 @@ function App() {
                         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
                         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
                     </div>
-
-                    <StepMessage step={step}>{messages[step - 1]}</StepMessage>
-
+                    <p className="message">
+                        Step {step}: {messages[step - 1]}
+                    </p>
                     <div className="buttons">
                         <Button
                             textColor="#fff"
