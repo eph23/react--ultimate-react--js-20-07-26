38411d5a5a1a5a330aa83deeb395ef635de04970
Eph23 <ephraims.818@gmail.com>
2026-08-07 06:32:42 +0600

    REACT--ULTIMATE-COURSE - TIR-State Management - Exercise - Accordion Component v1

---
+++ 03. TIR-state management/accordion-component-v1/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import data from "./data";
+
+function Accordion() {
+    return (
+        <div className="accordion">
+            {data.map((item, index) => (
+                <AccordionItem
+                    key={index + 1}
+                    title={item.title}
+                    text={item.text}
+                    num={index}
+                />
+            ))}
+        </div>
+    );
+}
+
+function AccordionItem({ num, title, text }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function handleToggle() {
+        setIsOpen((isOpen) => !isOpen);
+    }
+
+    return (
+        <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+            <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+            <p className="title">{title}</p>
+            <p className="icon">{isOpen ? "-" : "+"}</p>
+            {isOpen && <div className="content-box">{text}</div>}
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <Accordion />
+        </div>
+    );
+}
+
+export default App;
