e50d9437f1e814a393875e51cd53f927834fee4f
Eph23 <ephraims.818@gmail.com>
2026-08-20 21:01:59 +0600

    REACT--ULTIMATE-COURSE - TIR-Components, Composition, Reusability - 09. Improving Reusability with Props

--- 04. TIR-components, composition, reusability/popcorn/src/StarRating-03-hover.jsx
+++ 04. TIR-components, composition, reusability/popcorn/src/StarRating-03-hover.jsx
@@ -10,21 +10,13 @@ const starContainerStyle = {
     display: "flex",
 };
 
-function StarRating({
-    maxRating = 5,
-    color = "#fcc419",
-    size = 48,
-    messages = [],
-    defaultRating = 0,
-}) {
-    const textStyle = {
-        lineHeight: "1",
-        margin: "0",
-        color,
-        fontSize: `${size / 1.5}px`,
-    };
+const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+};
 
-    const [rating, setRating] = useState(defaultRating);
+function StarRating({ maxRating = 5 }) {
+    const [rating, setRating] = useState(0);
     const [tempRating, setTempRating] = useState(0);
 
     function handleRating(rating) {
@@ -45,28 +37,22 @@ function StarRating({
                         onRate={() => handleRating(index + 1)}
                         onHoverIn={() => setTempRating(index + 1)}
                         onHoverOut={() => setTempRating(0)}
-                        color={color}
-                        size={size}
                     />
                 ))}
             </div>
-            <p style={textStyle}>
-                {messages.length === maxRating
-                    ? messages[tempRating ? tempRating - 1 : rating - 1]
-                    : tempRating || rating || ""}
-            </p>
+            <p style={textStyle}>{tempRating || rating || ""}</p>
         </div>
     );
 }
 
-function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
-    const starStyle = {
-        width: `${size}px`,
-        height: `${size}px`,
-        display: "block",
-        cursor: "pointer",
-    };
+const starStyle = {
+    width: "48px",
+    height: "48px",
+    display: "block",
+    cursor: "pointer",
+};
 
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
     return (
         <span
             role="button"
@@ -79,8 +65,8 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
-                    fill={color}
-                    stroke={color}
+                    fill="gold"
+                    stroke="gold"
                 >
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
@@ -89,7 +75,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke={color}
+                    stroke="#000"
                 >
                     <path
                         strokeLinecap="round"
